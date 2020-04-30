527eee975394779db51281fbb4095b4e28ac481d
LloydGhettoWolf <lloyd.crawley@gmail.com>
2020-04-30 17:02:56 +0200

    some changes

--- Rustendo/src/main.rs
+++ Rustendo/src/main.rs
@@ -1,3 +1,12 @@
+use std::env;
+use std::fs;
+use std::io::Read;
+
 fn main() {
-    println!("Hello, world!");
+    let file_name = env::args().nth(1).unwrap();
+
+    let mut file = fs::File::open("LoZ.z64").unwrap();
+    let mut file_buf = Vec::new();
+    file.read_to_end(&mut file_buf);
+    println!("{}", file_name);
 }
