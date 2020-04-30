1b0064bf177535c5a195a7178a09c101449237f3
LloydGhettoWolf <lloyd.crawley@gmail.com>
2020-04-30 17:12:10 +0200

    load_binary function added

--- Rustendo/src/main.rs
+++ Rustendo/src/main.rs
@@ -1,12 +1,20 @@
 use std::env;
 use std::fs;
 use std::io::Read;
+use std::path::Path;
 
 fn main() {
-    let file_name = env::args().nth(1).unwrap();
+    let mut args = env::args();
+    let pif_file_name = args.nth(1).unwrap();
+    let rom_file_name = args.nth(2).unwrap();    
 
-    let mut file = fs::File::open("LoZ.z64").unwrap();
+    let pif_rom = load_binary(pif_file_name);
+    let bin_rom = load_binary(rom_file_name);
+}
+
+fn load_binary<P: AsRef<Path>>(path : P) -> Vec<u8> {
+    let mut file = fs::File::open(path).unwrap();
     let mut file_buf = Vec::new();
     file.read_to_end(&mut file_buf);
-    println!("{}", file_name);
+    file_buf
 }
